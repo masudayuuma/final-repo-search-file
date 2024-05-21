15bc9ddaace8b9fcf775e43280a4619269ad28e2
masudayuuma <ri0155sk@ed.ritsumei.ac.jp>
2024-05-21 09:18:46 +0900

    アカウント情報ページ

---
+++ src/pages/myacount/[user]/index.tsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import Link from "next/link";
+import styles from "@/styles/styles.module.css";
+import axios from "axios";
+import { userState } from "@/utils/atom";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
+
+interface Repo {
+  id: number;
+  full_name: string;
+  html_url: string;
+}
+
+const MyAccount: React.FC = () => {
+  // setUserDataを削除するとエラーが発生します
+  const [userData, setUserData] = useRecoilState(userState);
+  const [userRepos, setUserRepos] = useState<Repo[]>([]);
+
+  useEffect(() => {
+    if (userData) {
+      axios
+        .get(userData.repos_url)
+        .then((res) => setUserRepos(res.data))
+        .catch((error) => console.error(error));
+    }
+  }, [userData]);
+
+  return (
+    <>
+      <Header />
+      <main className={styles.main}>
+        <div>
+          <h1>マイアカウント詳細</h1>
+          {userData && (
+            <>
+              <p>アカウント名 {userData.login}</p>
+              <img src={userData.avatar_url} alt="image" />
+              {userData && <p>フォロワー数: {userData.followers}</p>}
+              {userData && <p>フォロー数: {userData.following}</p>}
+              {userRepos && (
+                <div>
+                  <h2>レポジトリ一覧</h2>
+                  <ul>
+                    {userRepos.map((repo) => (
+                      <li key={repo.id}>
+                        <Link href={repo.html_url}>{repo.full_name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+              <p>マイアカウントページ {userData.url}</p>
+            </>
+          )}
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export default MyAccount;
