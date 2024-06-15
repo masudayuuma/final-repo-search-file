9f1e337ea4a0f849eb735361b953f2a5986d5dd2
masudayuuma <ri0155sk@ed.ritsumei.ac.jp>
2024-06-15 21:16:36 +0900

    戻るボタンを押したときに一つ前のページに遷移する

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,25 +4,31 @@ import React, { useEffect } from "react";
 import { useRecoilState, useRecoilValue } from "recoil";
 import axios from "axios";
 import { usernameState, userDataState } from "@/utils/atom";
+import { useRouter } from 'next/router';
 
 export const Header: React.FC = () => {
   const [userData, setUserData] = useRecoilState(userDataState);
   const username = useRecoilValue(usernameState);
+  const router = useRouter();
 
   useEffect(() => {
     if (username) {
       axios.get(`https://api.github.com/users/${username}`)
         .then((res) => { setUserData(res.data) })
-        .catch((error) => console.error("ユーザーデータが取得できませんでした",error));
+        .catch((error) => console.error("ユーザーデータが取得できませんでした", error));
     }
   }, [username]);
+
+  const goBack = () => {
+    router.back();
+  };
 
   return (
     <header>
       <div className={styles.header}>
-        <Link href="/home">
-          <p>&larr; 戻る</p>
-        </Link>
+        <button onClick={goBack} className={styles.backButton}>
+          &larr; 戻る
+        </button>
         {userData && userData.avatar_url && (
           <Link href={`/myAccount/${username}`}>
             <img src={userData.avatar_url} alt="アイコン" className={styles.headerIcon} />
